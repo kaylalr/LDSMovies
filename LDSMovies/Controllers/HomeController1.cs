df2626ad179c4dc5766c4540fb67f895af64b787
Kayla Roberts <klr3of8@gmail.com>
2018-11-20 08:38:29 -0700

    Finished Tutorial

---
+++ LDSMovies/Controllers/HomeController1.cs
@@ -0,0 +1,48 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+
+//// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+//namespace LDSMovies.Controllers
+//{
+//	public class HomeController1 : Controller
+//	{
+//		// GET: /<controller>/
+//		public IActionResult Index()
+//		{
+//			return View();
+//		}
+//	}
+//}
+
+
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace HelloWorld.Controllers
+{
+	public class HelloWorldController : Controller
+	{
+		// 
+		// GET: /Index/
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		//
+		// GET: /Movies/Welcome/ 
+		// Requires using System.Text.Encodings.Web;
+		public IActionResult Welcome(string name, int numTimes = 1)
+		{
+			ViewData["Message"] = "Hello " + name;
+			ViewData["NumTimes"] = numTimes;
+
+			return View();
+		}
+	}
+}
---
+++ LDSMovies/Models/SeedData.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace LDSMovies.Models
+{
+	public static class SeedData
+	{
+		public static void Initialize(IServiceProvider serviceProvider)
+		{
+			using (var context = new LDSMoviesContext(
+				serviceProvider.GetRequiredService<DbContextOptions<LDSMoviesContext>>()))
+			{
+				// Look for any movies.
+				if (context.Movie.Any())
+				{
+					return;   // DB has been seeded
+				}
+
+				context.Movie.AddRange(
+					 new Movie
+					 {
+						 Title = "Once I Was a Beehive",
+						 ReleaseDate = DateTime.Parse("2015-8-14"),
+						 Genre = "Comedy",
+						 Rating = "PG",
+						 Price = 7.99M
+					 },
+
+					 new Movie
+					 {
+						 Title = "17 Miracles",
+						 ReleaseDate = DateTime.Parse("2011-6-3"),
+						 Genre = "Comedy",
+						 Rating = "PG",
+						 Price = 8.99M
+					 },
+
+					 new Movie
+					 {
+						 Title = "Meet The Mormons",
+						 ReleaseDate = DateTime.Parse("2014-10-10"),
+						 Genre = "Comedy",
+						 Rating = "PG",
+						 Price = 9.99M
+					 },
+
+				   new Movie
+				   {
+					   Title = "Pride and Prejudice: A Romantic Comedy",
+					   ReleaseDate = DateTime.Parse("2003-12-5"),
+					   Genre = "Romantic Comedy",
+					   Rating = "PG",
+					   Price = 3.99M
+				   },
+
+				   new Movie
+				   {
+					   Title = "Test",
+					   ReleaseDate = DateTime.Parse("2003-12-5"),
+					   Genre = "Romantic Comedy",
+					   Rating = "PG",
+					   Price = 3.99M
+				   }
+				);
+				context.SaveChanges();
+			}
+		}
+	}
+}
